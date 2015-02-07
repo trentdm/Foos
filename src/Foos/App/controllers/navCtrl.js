d288dc7eecc259703dcce6342cfaffb0c0d31317
Trent Miller <trentdm@gmail.com>
2015-02-07 12:47:02 -0700

    Fix for auth and nav sign in/out

--- src/Foos/App/controllers/navCtrl.js
+++ src/Foos/App/controllers/navCtrl.js
@@ -1,23 +1,14 @@
 ﻿angular.module('app').controller('NavCtrl',
     ['$scope', '$state', 'authModal', 'authService',
-        function ($scope, $state, authModal, authService) {
-            $scope.isActive = function (tab) {
+        function($scope, $state, authModal, authService) {
+            $scope.isActive = function(tab) {
                 return tab == $state.current.name;
             };
 
             $scope.user = authService.user;
-            
-            $scope.signin = function () {
-                authModal();
 
-                //for if I want to do routing after nav signin, 
-                //  currently just maintaining state
-                //    .then(function () {
-                //    return $state.go('play');
-                //})
-                //    .catch(function() {
-                //        return $state.go('home');
-                //    });
+            $scope.signin = function() {
+                authModal();
             };
 
             $scope.signout = function() {
